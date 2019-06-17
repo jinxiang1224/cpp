0f6747bf1911a71076b271d99286a0a0b1788751
849058411@qq.com <849058411@qq.com>
2019-06-17 23:14:48 +0800

    modify demo

--- project/src/UsingDllAPI/UsingDllAPI.cpp
+++ project/src/UsingDllAPI/UsingDllAPI.cpp
@@ -8,9 +8,14 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    //case1 vs2008 若crt有一个是MT，跨模块释放和分配会崩溃
+    //但在vs 2013不存在这个问题，有可能vs解决了该问题
     const int MAX_MALLOC_NUM = 5;
     unsigned int usSize = sizeof(int)* MAX_MALLOC_NUM;
     int* pnArray = NULL;
+
+    bool bCrossModule = false;
+    bool bUsingSTLCrossModule = true;
 
     if (!XDll_Malloc_Memory(usSize, (void**)&pnArray))
     {
@@ -21,26 +26,32 @@ int _tmain(int argc, _TCHAR* argv[])
     for (int i = 0; i < MAX_MALLOC_NUM; i++)
     {
         pnArray[i] = i + 1;
-    }
-
-    for (int i = 0; i < MAX_MALLOC_NUM; i++)
-    {
         printf("array[%d] = %d\n", i, pnArray[i]);
     }
 
-    if (!XDll_Free_Memory((void**)&pnArray))
+    if (!bCrossModule)
     {
-        printf("free memory error...\n");
+        if (!XDll_Free_Memory((void**)&pnArray))
+        {
+            printf("free memory error...\n");
+        }
+    }
+    else
+    {
+        if (NULL != pnArray)
+        {
+            free(pnArray);
+            pnArray = NULL;
+        }
     }
 
-    if (NULL != pnArray)
+    //case2  vs2008和2013均会崩溃 不管crt是MT/MD
+    if (bUsingSTLCrossModule)
     {
-        free(pnArray);
-        pnArray = NULL;
+        //设计内存重新分配
+        std::string strInfo;
+        XDll_Assign_String(strInfo);
     }
-
-
-
     return 0;
 }
 
