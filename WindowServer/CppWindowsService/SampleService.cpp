a9ce573f08d0b1f8f5da05bcf4fcee24b6583b9f
849058411@qq.com <849058411@qq.com>
2019-09-28 16:24:22 +0800

    add window servers demo

---
+++ WindowServer/CppWindowsService/SampleService.cpp
@@ -0,0 +1,91 @@
+/****************************** Module Header ******************************\
+* Module Name:  SampleService.cpp
+* Project:      CppWindowsService
+* Copyright (c) Microsoft Corporation.
+* 
+* Provides a sample service class that derives from the service base class - 
+* CServiceBase. The sample service logs the service start and stop 
+* information to the Application event log, and shows how to run the main 
+* function of the service in a thread pool worker thread.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+#pragma region Includes
+#include "SampleService.h"
+#pragma endregion
+
+
+CSampleService::CSampleService(PWSTR pszServiceName, 
+                               BOOL fCanStop, 
+                               BOOL fCanShutdown, 
+                               BOOL fCanPauseContinue)
+: CServiceBase(pszServiceName, fCanStop, fCanShutdown, fCanPauseContinue)
+{
+
+}
+
+
+CSampleService::~CSampleService(void)
+{
+
+}
+
+
+//
+//   FUNCTION: CSampleService::OnStart(DWORD, LPWSTR *)
+//
+//   PURPOSE: The function is executed when a Start command is sent to the 
+//   service by the SCM or when the operating system starts (for a service 
+//   that starts automatically). It specifies actions to take when the 
+//   service starts. In this code sample, OnStart logs a service-start 
+//   message to the Application log, and queues the main service function for 
+//   execution in a thread pool worker thread.
+//
+//   PARAMETERS:
+//   * dwArgc   - number of command line arguments
+//   * lpszArgv - array of command line arguments
+//
+//   NOTE: A service application is designed to be long running. Therefore, 
+//   it usually polls or monitors something in the system. The monitoring is 
+//   set up in the OnStart method. However, OnStart does not actually do the 
+//   monitoring. The OnStart method must return to the operating system after 
+//   the service's operation has begun. It must not loop forever or block. To 
+//   set up a simple monitoring mechanism, one general solution is to create 
+//   a timer in OnStart. The timer would then raise events in your code 
+//   periodically, at which time your service could do its monitoring. The 
+//   other solution is to spawn a new thread to perform the main service 
+//   functions, which is demonstrated in this code sample.
+//
+void CSampleService::OnStart(DWORD dwArgc, LPWSTR *lpszArgv)
+{
+    // Log a service start message to the Application log.
+   WriteEventLogEntry(L"CppWindowsService in OnStart", 
+      EVENTLOG_INFORMATION_TYPE);
+    // Queue the main service function for execution in a worker thread.
+    //CThreadPool::QueueUserWorkItem(&CSampleService::ServiceWorkerThread, this);
+}
+
+//
+//   FUNCTION: CSampleService::OnStop(void)
+//
+//   PURPOSE: The function is executed when a Stop command is sent to the 
+//   service by SCM. It specifies actions to take when a service stops 
+//   running. In this code sample, OnStop logs a service-stop message to the 
+//   Application log, and waits for the finish of the main service function.
+//
+//   COMMENTS:
+//   Be sure to periodically call ReportServiceStatus() with 
+//   SERVICE_STOP_PENDING if the procedure is going to take long time. 
+//
+void CSampleService::OnStop()
+{
+    WriteEventLogEntry(L"CppWindowsService in OnStop", 
+        EVENTLOG_INFORMATION_TYPE);
+}
