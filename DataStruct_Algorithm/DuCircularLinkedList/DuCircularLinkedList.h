a33b9e266126cecdeb91546cda5225a674673bb1
849058411@qq.com <849058411@qq.com>
2019-11-17 20:09:06 +0800

    modify head file

--- DataStruct_Algorithm/DuCircularLinkedList/DuCircularLinkedList.h
+++ DataStruct_Algorithm/DuCircularLinkedList/DuCircularLinkedList.h
@@ -16,7 +16,7 @@ typedef struct _DuLNode
 typedef struct
 {
     int     nLenth; /*链表长度*/
-    PDuLNode Head;  /*链表头结点*/
+    PDuLNode pHead;  /*链表头结点*/
 }DuLNodeInfo;
 
 /*初始化双向循环链表*/
@@ -28,19 +28,19 @@ int ClearDuLinkList(DuLNodeInfo& ListInfo);
 /*销毁链表*/
 int DestoryDuLinkList(DuLNodeInfo& ListInfo);
 
-/*添加一个结点*/
-int AddNode(PDuLNode newnode, PDuLNode pre, PDuLNode next);
+/*添加一个结点,结点本身已存在*/
+int ListAddNode(DuLNodeInfo& ListInfo,PDuLNode newnode, PDuLNode pre, PDuLNode next);
 
-/*将当前结点从链表中删除*/
-int DelNode(PDuLNode delNode);
+/*删除一个结点，结点本身仍存在*/
+int ListDelNode(DuLNodeInfo& ListInfo,PDuLNode delNode);
 
 /*获取链表长度*/
 int GetDuListLen(DuLNodeInfo& ListInfo);
 
-/*插入指定结点前*/
+/*在头结点后插入，新生成的结点*/
 int InsertBeforeLinkList(DuLNodeInfo& ListInfo, NodeType data);
 
-/*插入指定结点后*/
+/*追加结点，新生成的结点*/
 int InsertAfterLinkList(DuLNodeInfo& ListInfo, NodeType data);
 
 /*获取头结点*/
@@ -49,11 +49,14 @@ PDuLNode GetTailNode(const DuLNodeInfo& ListInfo);
 /*获取尾结点*/
 PDuLNode GetHeadNode(const DuLNodeInfo& ListInfo);
 
-/*删除结点*/
+/*删除结点并释放资源*/
 int DelDuLinkListNode(DuLNodeInfo& ListInfo,PDuLNode& delnode);
 
 /*链表查找*/
-int FindDuLinkListNode(DuLNodeInfo& ListInfo, PDuLNode& FindNode,NodeType DataValue);
+PDuLNode FindDuLinkListNode(DuLNodeInfo& ListInfo,NodeType DataValue);
+
+/*链表打印*/
+int UnionDuLinkList(DuLNodeInfo& ListInfo1, DuLNodeInfo& ListInfo2);
 
 /*链表打印*/
 int PrintDuLinkList(DuLNodeInfo& ListInfo, bool bReverse = false);
